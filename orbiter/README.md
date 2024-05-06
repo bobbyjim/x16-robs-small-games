205bd86d1c42cf4246e46dbb12259fefed34f5c4
Robert Eaglestone <robert.eaglestone@gmail.com>
2024-05-05 22:00:43 -0500

    task: added README and #defines

---
+++ orbiter/README.md
@@ -0,0 +1,5 @@
+ORBITER - a PETSCII space station simulator
+
+# Components
+
+Each station component is a 3x3 matrix of PETSCII characters.
--- orbiter/main.c
+++ orbiter/main.c
@@ -2,6 +2,14 @@
 
 #include "universal.h"
 
+#define     OPEN_CIRCLE    215
+#define     CROSS          219
+#define     CURVE_NW       213
+#define     CURVE_NE       201
+#define     CURVE_SW       202
+#define     CURVE_SE       203
+#define     HORIZ          192
+
 void main()
 {
    setPetFont();
